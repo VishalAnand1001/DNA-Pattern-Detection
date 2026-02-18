70d2d8701df0505479a0239f4a849e0032feaa91
VishalAnand1001 <vishalsg1001@gmail.com>
2026-02-18 21:57:36 +0530

    performance files updated

--- performance.cpp
+++ performance.cpp
@@ -1,1 +1,17 @@
+#include "performance.h"
 #include <chrono>
+
+double measureExecutionTime(const std::string& text, 
+                            const std::string& pattern, 
+                            MatchResult (*searchFunc)(const std::string&, const std::string&)) {
+    
+    auto start = std::chrono::high_resolution_clock::now();
+    
+    
+    searchFunc(text, pattern);
+    
+    auto end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double, std::milli> duration = end - start;
+    
+    return duration.count(); 
+}
--- performance.h
+++ performance.h
@@ -1,3 +1,14 @@
 #ifndef PERFORMANCE_H
 #define PERFORMANCE_H
+
+#include <string>
+#include <functional>
+#include "dna_matcher.h"
+
+double measureExecutionTime(const std::string& text, 
+                            const std::string& pattern, 
+                            MatchResult (*searchFunc)(const std::string&, const std::string&));
+
 #endif
+
+
